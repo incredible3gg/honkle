5f9a645aa9615743ffff165ea57b50df00aa204c
incredible3gg <63173638+incredible3gg@users.noreply.github.com>
2021-11-04 14:49:16 -0700

    OP UPDATE ####UPDATE
    added new arrays for leveling up

--- honkle.js
+++ honkle.js
@@ -13,9 +13,10 @@ const rl = readline.createInterface({
 })
 
 const print = console.log;
-const trash = [
+var trash = []
+var lvl0trash = [
 	/*THE TRASH RULES*/
-	//the points must be between 2 and 19
+	//the points must be between 2 and 19, unless it is a negitave score in which case the range is -5 to -10
 	//the rarer it is to find in a real trash pile or the more nutricios it is to a honkle defines the point value
 	{name:"a tin can",points:5},
 	{name:"an old tire",points:8},
@@ -29,8 +30,35 @@ const trash = [
 	{name:"a baseball hat",points:4},
 	{name:"some rotting meat",points:18},
 	{name:"a suitcase",points:6},
+	{name:"an agate",points:-3},
+	{name:"some pages from a book called 'Essentials of Physics'",points:7}
 ]
-
+var lvl1trash = [
+	/*LEVEL 1 TRASH RULES*/
+	//the points must be between 5 and 23, with no negative point trash from this level and up.
+	//you can only add 3 items with points over 20 (including the ones already added) and all of the items have to be common household items that are not normaly found in trash piles.
+	{name:"a cd player",points:10},
+	{name:"a coke bottle",points:13},
+	{name:"a sodden sock",points:23},
+	{name:"a cowboy boot",points:19},
+	{name:"some watermelon rinds",points:14},
+	{name:"flour",points:5},
+	{name:"a game controller",points:10}
+],
+var lvl2trash = [
+	/*LEVEL 2 TRASH RULES*/
+	//The points for an item must be between 10 and 28.
+	//You can only add 5 items above 25 and all of the items have to be somewhat expensive, but not too expensive.
+	{name:"an ancient artifact",points:28},
+	{name:"a computer",points:19},
+	{name:"guitar",points:20},
+	{name:"a tire full of money",points:23},
+	{name:"an expensive violin",points:15},
+	{name:"a winning lottery ticket",points:21},
+	{name:"a famous painting",points:26},
+	{name:"a wedding ring",points:14},
+	{name:"an AC unit",points:25}
+]
 var points = 0
 var notafood = 0
 var spintime = 1
@@ -43,27 +71,35 @@ function trashLoop() {
 		console.clear();		
 		if (points > 50) {
 			rank = "noob";
+			trash = [...lvl0trash]
 		} 
 		if (points > 200) {
 			rank = "afk pro";
+			trash = [...lvl0trash, ...lvl1trash]
 		} 
 		if (points > 500) {
 			rank = "pro";
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		}
 		if (points > 1000) {
 			rank = "couch potato"
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		} 
 		if (points > 2000) {
 			rank =  "NaN potato"
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		}
 		if (points > 5000) {
 			rank = "Lamy"
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		}
 		if (points > 10000) {
 			rank = "afk Sniffle"
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		}
 		if (points > 15000) {
 			rank = "Sniffle"
+			trash = [...lvl0trash,...lvl1trash,...lvl2trash]
 		}
 
 		spintime += 1
